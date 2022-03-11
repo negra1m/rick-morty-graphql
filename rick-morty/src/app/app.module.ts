15cd44d7962ec948991f1941a4fbf172542915da
negra1m <vnsn_@outlook.com>
2022-03-11 02:46:32 -0300

    :sparkles: finished main (missing components footer n buttons)

--- rick-morty/src/app/app.module.ts
+++ rick-morty/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { MainPageComponent } from './main-page/main-page.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
