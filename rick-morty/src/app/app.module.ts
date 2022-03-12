526a3dcb5325e211e0c33e003f8565a7807f6a57
negra1m <vnsn_@outlook.com>
2022-03-11 21:00:25 -0300

    :sparkles: creating card component and playground

--- rick-morty/src/app/app.module.ts
+++ rick-morty/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { CardComponent } from './components/card/card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
---
+++ rick-morty/src/app/components/card/card/card.component.sandbox.ts
@@ -0,0 +1,7 @@
+import { sandboxOf } from 'angular-playground';
+import { CardComponent } from '../card.component';
+
+export default sandboxOf(CardComponent)
+  .add('default', {
+    template: `<app-card></app-card>`
+  });
